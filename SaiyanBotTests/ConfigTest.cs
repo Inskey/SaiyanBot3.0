b2de4e853ee7008e813f47614ec987d07276f5b6
skidsirongenesisdev@gmail.com <skidsirongenesisdev@gmail.com>
2017-03-05 18:02:20 +0000

    Added ParseConfig unit test

--- SaiyanBotTests/ConfigTest.cs
+++ SaiyanBotTests/ConfigTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using SaiyanBot3_0.Configuration;
@@ -11,7 +11,37 @@ namespace SaiyanBotTests
         [TestMethod]
         public void ParseConfigTest()
         {
+            // Create dummy config file contents
+            StringBuilder sbuild = new StringBuilder();
+            sbuild.AppendLine("[Twitch]");
+            sbuild.AppendLine("user=SaiyanBot");
+            sbuild.AppendLine("pass=1234pass");
+            sbuild.AppendLine("channels=");
+            sbuild.AppendLine("\tgunnerwolfgaming");
+            sbuild.AppendLine("    skidsdev");
+            sbuild.AppendLine("\ttearrotime");
+            sbuild.AppendLine("[Discord]");
+            sbuild.AppendLine("token=12345678abcdef");
 
+            // Parse dummy config
+            Config newConf = Config.ParseConfig(sbuild.ToString());
+
+            bool assertion = true;
+
+            // Valid Twitch config
+            assertion = (assertion && newConf.IsTwitch);
+            // Valid Discord config
+            assertion = (assertion && newConf.IsDiscord);
+            // Twitch username correctly parsed
+            assertion = (assertion && newConf.Twitch.User == "SaiyanBot");
+            // Twitch channels correctly parsed
+            assertion = (assertion && newConf.Twitch.Channels.Count == 3);
+            assertion = (assertion && newConf.Twitch.Channels[0] == "gunnerwolfgaming");
+            assertion = (assertion && newConf.Twitch.Channels[1] == "skidsdev");
+            assertion = (assertion && newConf.Twitch.Channels[2] == "tearrotime");
+
+            // Assert
+            Assert.IsTrue(assertion);
         }
     }
 }
