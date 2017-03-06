09ea1e95240e9cf948bea5639563bb981c58250f
skidsirongenesisdev@gmail.com <skidsirongenesisdev@gmail.com>
2017-03-06 03:36:17 +0000

    Added TwitchConfig public properties and constructor

--- SaiyanBot3_0/Configuration/TwitchConfig.cs
+++ SaiyanBot3_0/Configuration/TwitchConfig.cs
@@ -8,5 +8,15 @@ namespace SaiyanBot3_0.Configuration
 {
     public class TwitchConfig
     {
+        public string User { get; private set; }
+        public string Pass { get; private set; }
+        public string[] Channels { get; private set; }
+
+        public TwitchConfig(string user, string pass, List<string> channels)
+        {
+            User = user;
+            Pass = pass;
+            Channels = channels.ToArray();
+        }
     }
 }
