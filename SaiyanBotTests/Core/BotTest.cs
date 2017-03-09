c0fc3d589078501e07828c6ad200ef4327bd125f
Dylan Renwick <dylan.renwick96@gmail.com>
2017-03-09 14:44:58 +0000

    Added Bot class

---
+++ SaiyanBotTests/Core/BotTest.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SaiyanBot3_0.Configuration;
+
+namespace SaiyanBotTests.Core
+{
+    [TestClass]
+    public class BotTest
+    {
+        
+    }
+}
