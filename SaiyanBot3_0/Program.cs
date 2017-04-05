60460cc49222b7716f7746c878643e3108aada66
Dylan Renwick <skidsirongenesisdev@gmail.com>
2017-04-05 09:01:02 +0100

    Added config loading to entry method

--- SaiyanBot3_0/Program.cs
+++ SaiyanBot3_0/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,31 @@ namespace SaiyanBot3_0
     {
         static void Main(string[] args)
         {
+        	string configPath = "/saiyanbot.cfg";
+        	if (args.Length > 0)
+        	{
+        		if (args[0] == "-c" || args[0] == "--config")
+        		{
+        			if (args.Length < 2)
+        			{
+        				Console.WriteLine($"Invalid value after flag \"{args[0]}\"");
+        				return;
+        			}
+
+        			configPath = args[1];
+        		}
+        		else if (args[0] == "-h" || args[0] == "--help")
+        		{
+        			Console.WriteLine("Options:");
+        			Console.WriteLine("    -c  --config: Specify a different configuration file directory");
+        			Console.WriteLine("    -h  --help:   Show this info");
+        		}
+        	}
+
+        	Config conf = Config.ParseConfig(Config.LoadConfigFile(File.GetDirectory(Assembly.GetExecutingAssembly.Location) + configPath));
+
+        	Bot bot = new Bot(conf);
+
         }
     }
 }
