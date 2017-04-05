bb84ec80ea669ee5938dcd0a15aaa132a479ee6b
Dylan Renwick <skidsirongenesisdev@gmail.com>
2017-04-05 09:10:43 +0100

    Added CommandController class

---
+++ SaiyanBot3_0/Command/CommandController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SaiyanBot3_0.Command
+{
+	public class CommandController
+	{
+		private List<Command> commands;
+
+		public List<Command> DiscordCommands
+		{
+			get
+			{
+				return commands.Where(x => x.IsDiscord);
+			}
+		}
+		public List<Command> TwitchCommands
+		{
+			get
+			{
+				return commands.Where(x => x.IsTwitch);
+			}
+		}
+
+		public CommandController()
+		{
+			
+		}
+	}
+}
